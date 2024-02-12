7e6ddc13296d4cadd8f3e8832381bc0d8c40b185
Andrew Rarijason <rarijasonaiky@gmail.com>
2024-02-12 23:52:51 +0300

    Update Connexion.php

--- models/Connexion.php
+++ models/Connexion.php
@@ -1,10 +1,17 @@
 <?php
-function connexion(){
-		static $connect = null;
-		if ($connect===null) {
-			$connect = mysqli_connect('localhost','root','Andrew2963','the');
-		}else{
-		}
-		return $connect;
-	}
+    function connexion() 
+    {
+        $serveur = "localhost";
+        $utilisateur = "root";
+        $motDePasse = "Andrew2963";
+        $baseDeDonnees = "the";
+
+        $connexion = new mysqli($serveur, $utilisateur, $motDePasse, $baseDeDonnees);
+
+        if ($connexion->connect_error) 
+        {
+            die("Erreur lors de la connexion : " . $connexion->connect_error);
+        }
+        return $connexion;
+    }
 ?>
