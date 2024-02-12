3adbf02e62842b230d071845f13637a8f26196d1
Andrew Rarijason <rarijasonaiky@gmail.com>
2024-02-12 23:52:18 +0300

    Create insertionSalaire.php

---
+++ controller/insertionSalaire.php
@@ -0,0 +1,32 @@
+<?php
+
+include ('../models/fonctions.php');
+
+// Vérifier si des données ont été soumises via le formulaire
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    // Vérifier si les champs du formulaire sont définis et non vides
+    if (isset($_GET["cueilleurs"]) && isset($_GET["Montant"]) &&
+        !empty($_GET["cueilleurs"]) && !empty($_GET["Montant"])) {
+
+        // Récupérer les données du formulaire
+        $type = $_GET["cueilleurs"];
+        $Montant = $_GET["Montant"];
+
+        // Insérer les données dans la base de données
+        $colonnes = "id_cueilleur, montant";
+        $valeurs = "'$type', '$Montant'";
+        insertion("salaires_cueilleurs", $colonnes, $valeurs); 
+
+        // Rediriger vers une autre page après l'insertion réussie
+        header("Location: success.php");
+        exit();
+    } else {
+        // Afficher un message d'erreur si des champs sont manquants dans le formulaire
+        echo "Tous les champs du formulaire sont obligatoires.";
+    }
+} else {
+    // Rediriger vers une autre page si le formulaire n'a pas été soumis via la méthode GET
+    header("Location: index.php");
+    exit();
+}
+?>
