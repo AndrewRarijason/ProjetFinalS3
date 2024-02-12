1f15a44d5b3f5d259629d5efee1c37295169f4d9
Koulina <ravonilantosoakoloinasarobidy@gmail.com>
2024-02-12 20:23:43 +0300

    Update Connexion.php

--- Connexion.php
+++ Connexion.php
@@ -1,3 +1,17 @@
 <?php
-    //
+    function connexion() 
+    {
+        $serveur = "localhost";
+        $utilisateur = "root";
+        $motDePasse = "";
+        $baseDeDonnees = "";
+
+        $connexion = new mysqli($serveur, $utilisateur, $motDePasse, $baseDeDonnees);
+
+        if ($connexion->connect_error) 
+        {
+            die("Erreur lors de la connexion : " . $connexion->connect_error);
+        }
+        return $connexion;
+    }
 ?>
