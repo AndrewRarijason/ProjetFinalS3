571073811a011fad93a8e85e00ddc0bb81bbab5c
Andrew Rarijason <rarijasonaiky@gmail.com>
2024-02-13 17:37:30 +0300

    Update traitementAdmin.php

--- controller/traitementAdmin.php
+++ controller/traitementAdmin.php
@@ -7,7 +7,7 @@ if(isset($_POST['username']) && isset($_POST['password'])) {
     $password = $_POST['password'];
 
     // Appeler la fonction validLogin
-    if(validLogin($username, $password, 'admin')) { // 'admin' comme type d'utilisateur
+    if(validLogin($username, $password, 'Admin')) { // 'admin' comme type d'utilisateur
         // Redirection vers la page de succès après un login réussi
         header("Location: ../views/admin.html");
         exit(); // Assurez-vous d'arrêter l'exécution du script après la redirection
