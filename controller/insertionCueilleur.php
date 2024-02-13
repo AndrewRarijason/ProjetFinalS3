ea91782c71934edbb6fc5c495c9b6f4c6ba35439
Andrew Rarijason <rarijasonaiky@gmail.com>
2024-02-13 16:57:39 +0300

    Update insertionCueilleur.php

--- controller/insertionCueilleur.php
+++ controller/insertionCueilleur.php
@@ -18,8 +18,8 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         $valeurs = "'$nom', '$genre', '$Date'";
         insertion("cueilleurs", $colonnes, $valeurs); 
 
-        // Rediriger vers une autre page après l'insertion réussie
-        header("Location: success.php");
+        $filePath = '../views/success.php';
+            header("Location: $filePath");
         exit();
     } else {
         // Afficher un message d'erreur si des champs sont manquants dans le formulaire
