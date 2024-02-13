66e0e138efa4d359de85825d03ac990fa2bf44d1
Andrew Rarijason <rarijasonaiky@gmail.com>
2024-02-13 11:04:52 +0300

    Update tableUpdate.sql

--- base/tableUpdate.sql
+++ base/tableUpdate.sql
@@ -67,24 +67,35 @@ CREATE TABLE Cueillettes (
     FOREIGN KEY (id_parcelle) REFERENCES Parcelles(id)
 );
 
- CREATE TABLE regeneration(id primary key AUTO_INCREMENT, mois int);
- CREATE TABLE payement_cueilleurs
+ create table regeneration(id int AUTO_INCREMENT  primary key, mois int);
+ create table payement_cueilleurs
  (
-    id primary key AUTO_INCREMENT,
+    id int AUTO_INCREMENT  primary key,
     poids_min decimal,
     id_cueilleur int,
     bonus float,
-    mallus float
+    mallus float,
     FOREIGN KEY (id_cueilleur) REFERENCES Cueilleurs(id)
  );
 
  create table prix_variete
  (
-    id primary key AUTO_INCREMENT, 
+    id int AUTO_INCREMENT  primary key, 
     montant decimal, 
-    id_variete int 
+    id_variete int, 
     FOREIGN KEY (id_variete) REFERENCES Varietes_The(id)
  );
+
+CREATE OR REPLACE VIEW v_montant AS
+SELECT C.date_cueillette as date_cueillette, 
+       Cr.nom as nom, 
+       C.poids_cueilli as poids, 
+       P.bonus as bonus, 
+       P.mallus as mallus, 
+       P.poids_min as poids_min
+FROM Cueillettes C
+JOIN Cueilleurs Cr ON C.id_cueilleur = Cr.id 
+JOIN payement_cueilleurs P on P.id_cueilleur=Cr.id;
 
 CREATE VIEW affichage AS
 SELECT Varietes_The.id AS id_variete, Parcelles.id AS id_parcelle, Parcelles.surface
