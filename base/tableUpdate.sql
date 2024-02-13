25fbc5bd40f665a47770f0bbee65540642f18f1d
Andrew Rarijason <rarijasonaiky@gmail.com>
2024-02-13 16:57:27 +0300

    Update tableUpdate.sql

--- base/tableUpdate.sql
+++ base/tableUpdate.sql
@@ -1,6 +1,3 @@
-CREATE DATABASE the;
-use the;
-
 -- Table admin
 CREATE TABLE Admin(
     id_admin INT AUTO_INCREMENT PRIMARY KEY,
@@ -8,12 +5,17 @@ CREATE TABLE Admin(
     mdp VARCHAR (20) 
  );
 
+ insert into Admin(nom,mdp) values('Rasoa', 'rasoa');
+ insert into Admin(nom,mdp) values('Rabe', '');
+
  --Table utilisateur
  CREATE TABLE Utilisateur(
     id_utilisateur INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR (50),
     mdp VARCHAR (20) 
  );
+insert into Utilisateur(nom,mdp) values('Randria', 'randria');
+insert into Utilisateur(nom,mdp) values('Ravao', 'ravao');
 
 -- Table cueilleurs
 CREATE TABLE Cueilleurs (
@@ -22,8 +24,10 @@ CREATE TABLE Cueilleurs (
     genre VARCHAR(10),
     date_naissance DATE
 );
+insert into(nom, genre, date_naissance) values("Koloina", "Feminin", "2024-02-13");
 
--- Table variétés de thé
+
+-- Tables variétés de thé
 CREATE TABLE Varietes_The (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(255) NOT NULL,
@@ -32,9 +36,9 @@ CREATE TABLE Varietes_The (
 );
 
 -- Table parcelles (de thé)
-CREATE TABLE Parcelles (
+CREATE TABLE Parcelles 
+(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nom_parcelle VARCHAR(50),
     surface FLOAT,
     id_the INT,
     FOREIGN KEY (id_the) REFERENCES Varietes_The(id)
@@ -51,9 +55,7 @@ CREATE TABLE Depenses (
 -- Table salaires des cueilleurs
 CREATE TABLE Salaires_cueilleurs (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    id_cueilleur INT,
-    montant DECIMAL(8,2) NOT NULL,
-    FOREIGN KEY (id_cueilleur) REFERENCES cueilleurs(id)
+    montant DECIMAL(8,2) NOT NULL
 );
 
 -- Table pour les cueillettes
@@ -67,8 +69,11 @@ CREATE TABLE Cueillettes (
     FOREIGN KEY (id_parcelle) REFERENCES Parcelles(id)
 );
 
- create table regeneration(id int AUTO_INCREMENT  primary key, mois int);
- create table payement_cueilleurs
+--Table pour les regenerations par saison
+create table regeneration(id int AUTO_INCREMENT  primary key, mois int);
+
+--Table payement_cueilleurs
+create table payement_cueilleurs
  (
     id int AUTO_INCREMENT  primary key,
     poids_min decimal,
@@ -78,6 +83,7 @@ CREATE TABLE Cueillettes (
     FOREIGN KEY (id_cueilleur) REFERENCES Cueilleurs(id)
  );
 
+--Table pour le prix de vente des varietes de the
  create table prix_variete
  (
     id int AUTO_INCREMENT  primary key, 
@@ -86,7 +92,34 @@ CREATE TABLE Cueillettes (
     FOREIGN KEY (id_variete) REFERENCES Varietes_The(id)
  );
 
-CREATE OR REPLACE VIEW v_montant AS
+--Vues
+CREATE VIEW affichage AS
+SELECT Varietes_The.id AS id_variete, Parcelles.id AS id_parcelle, Parcelles.surface
+FROM Varietes_The
+JOIN Parcelles ON Parcelles.id_the = Varietes_The.id;
+
+CREATE OR REPLACE VIEW v_Prevision AS
+SELECT 
+   c.date_cueillette as date_cueillette,
+    p.id AS id_parcelle,
+    p.surface AS surface_totale_parcelle,
+    SUM(c.poids_cueilli) AS poids_total_cueilli,
+    (p.surface * 10000 / v.occupation) AS nombre_pieds_total,  -- Calcul du nombre total de pieds dans la parcelle
+    ((p.surface * 10000 / v.occupation) - SUM(c.poids_cueilli)) AS nombre_pieds_restants,  -- Calcul du nombre de pieds restants
+    (p.surface - (SUM(c.poids_cueilli) / v.rendement)) AS surface_occupee_restante,  -- Calcul de la surface occupée restante
+    ((p.surface * 10000 / v.occupation) - SUM(c.poids_cueilli)) * pr.montant AS cout_the_restant  -- Calcul du coût du thé restant
+FROM 
+    Parcelles p
+LEFT JOIN 
+    Varietes_The v ON p.id_the = v.id
+LEFT JOIN 
+     prix_variete pr on p.id_the=pr.id_variete
+LEFT JOIN
+    Cueillettes c ON p.id = c.id_parcelle 
+GROUP BY 
+    p.id;
+
+ CREATE OR REPLACE VIEW v_montant AS
 SELECT C.date_cueillette as date_cueillette, 
        Cr.nom as nom, 
        C.poids_cueilli as poids, 
@@ -96,8 +129,3 @@ SELECT C.date_cueillette as date_cueillette,
 FROM Cueillettes C
 JOIN Cueilleurs Cr ON C.id_cueilleur = Cr.id 
 JOIN payement_cueilleurs P on P.id_cueilleur=Cr.id;
-
-CREATE VIEW affichage AS
-SELECT Varietes_The.id AS id_variete, Parcelles.id AS id_parcelle, Parcelles.surface
-FROM Varietes_The
-JOIN Parcelles ON Parcelles.id_the = Varietes_The.id;
